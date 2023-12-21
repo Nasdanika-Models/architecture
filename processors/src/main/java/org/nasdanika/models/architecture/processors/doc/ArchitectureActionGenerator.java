6d6694545b75b180864444c52ae4eb00ef9a95cc
Pavel <Pavel.Vlasov@nasdanika.org>
2023-12-21 09:01:10 -0500

    ArchitectureActionGenerator, not yet tested.

--- processors/src/main/java/org/nasdanika/models/architecture/processors/doc/ArchitectureActionGenerator.java
+++ processors/src/main/java/org/nasdanika/models/architecture/processors/doc/ArchitectureActionGenerator.java
@@ -1,198 +1,59 @@
 package org.nasdanika.models.architecture.processors.doc;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
-import java.util.function.Consumer;
+import java.util.UUID;
+import java.util.function.Function;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.nasdanika.common.Context;
-import org.nasdanika.common.Diagnostic;
-import org.nasdanika.common.ProgressMonitor;
-import org.nasdanika.html.model.app.Action;
-import org.nasdanika.html.model.app.Label;
+import org.nasdanika.html.model.app.graph.emf.ActionGenerator;
 import org.nasdanika.models.architecture.ArchitecturePackage;
 import org.nasdanika.models.echarts.graph.GraphPackage;
 import org.nasdanika.ncore.NcorePackage;
 
-public class ArchitectureActionGenerator {
-	
-	protected ArchitectureNodeProcessorFactory nodeProcessorFactory;
-	
-	public ArchitectureActionGenerator() {
-		this(Context.EMPTY_CONTEXT, null);
+public class ArchitectureActionGenerator extends ActionGenerator<ArchitectureNodeProcessorFactory> {
+
+	public ArchitectureActionGenerator(
+			Collection<? extends EObject> sources,
+			ArchitectureNodeProcessorFactory nodeProcessorFactory, 
+			Collection<? extends EObject> references,
+			Function<? super EObject, URI> uriResolver) {
+		super(sources, nodeProcessorFactory, references, uriResolver);
 	}
 	
-	public ArchitectureActionGenerator(Context context, java.util.function.BiFunction<URI, ProgressMonitor, Action> prototypeProvider) {
-		this(new ArchitectureNodeProcessorFactory(context, prototypeProvider));
-	}
+	private static Map<EPackage, URI> REFERENCES = Map.ofEntries(
+			Map.entry(EcorePackage.eINSTANCE, URI.createURI("https://ecore.models.nasdanika.org/")),			
+			Map.entry(NcorePackage.eINSTANCE, URI.createURI("https://ncore.models.nasdanika.org/")),			
+			Map.entry(GraphPackage.eINSTANCE, URI.createURI("https://graph.models.nasdanika.org/")),			
+			Map.entry(ArchitecturePackage.eINSTANCE, URI.createURI("https://architecture.models.nasdanika.org/")));
 		
-	public ArchitectureActionGenerator(ArchitectureNodeProcessorFactory nodeProcessorFactory) {
-		this.nodeProcessorFactory = nodeProcessorFactory;
-	}
-		
-	/**
-	 * Generates actions for a single source with a baseURI
-	 * @param source
-	 * @param baseURI Base URI. Can be null. If null, a random URI is generated. Use base URI if you want to generate relative links to referenced objects. 
-	 * @param nodeProcessorFactory
-	 * @param references
-	 * @param diagnosticConsumer
-	 * @param progressMonitor
-	 * @return
-	 */
-	public Map<EObject,Collection<Label>> generateActionModel(
+	public ArchitectureActionGenerator(
 			EObject source,
 			URI baseURI,
-			Consumer<Diagnostic> diagnosticConsumer,
-			ProgressMonitor progressMonitor) {		
-		
-		return org.nasdanika.html.model.app.graph.emf.Util.generateActionModel(
-				source, 
-				baseURI, 
-				nodeProcessorFactory, 
-				getReferencedEPackages(), 
-				diagnosticConsumer, 
-				progressMonitor);
-		
+			ArchitectureNodeProcessorFactory nodeProcessorFactory) {
+		super(
+			Collections.singleton(source),
+			nodeProcessorFactory, 
+			REFERENCES.keySet(), 
+			eObj -> {
+				if (eObj == source) {
+					return baseURI;
+				}
+				return REFERENCES.get(eObj);
+			});
 	}
-
-	protected Map<EObject, URI> getReferencedEPackages() {
-		return Map.ofEntries(
-				Map.entry(EcorePackage.eINSTANCE, URI.createURI("https://ecore.models.nasdanika.org/")),			
-				Map.entry(NcorePackage.eINSTANCE, URI.createURI("https://ncore.models.nasdanika.org/")),			
-				Map.entry(GraphPackage.eINSTANCE, URI.createURI("https://graph.models.nasdanika.org/")),			
-				Map.entry(ArchitecturePackage.eINSTANCE, URI.createURI("https://architecture.models.nasdanika.org/"))			
-			);
+		
+	public ArchitectureActionGenerator(EObject source, ArchitectureNodeProcessorFactory nodeProcessorFactory) {
+		this(source, URI.createURI("tmp://" + UUID.randomUUID() + "/" + UUID.randomUUID() + "/"), nodeProcessorFactory);
 	}
 	
-	/**
-	 * Generates actions for a single source and saves to a resource identified by URI
-	 * @param source
-	 * @param baseURI
-	 * @param nodeProcessorFactory
-	 * @param references
-	 * @param diagnosticConsumer
-	 * @param actionModelResourceURI Resource URI
-	 * @param progressMonitor
-	 * @throws IOException
-	 */
-	public void generateActionModel(
-			EObject source,
-			URI baseURI,
-			Consumer<Diagnostic> diagnosticConsumer,
-			URI actionModelResourceURI,
-			ProgressMonitor progressMonitor) throws IOException {
-	
-		Map<EObject, Collection<Label>> labelMap = generateActionModel(
-				source,
-				baseURI,
-				diagnosticConsumer, 
-				progressMonitor);
-		
-		org.nasdanika.html.model.app.graph.emf.Util.saveLabelMap(labelMap, actionModelResourceURI);
-	}
-	
-	/**
-	 * Generates actions for a single source and saves to a file
-	 * @param source
-	 * @param baseURI
-	 * @param nodeProcessorFactory
-	 * @param references
-	 * @param diagnosticConsumer
-	 * @param actionModelFile
-	 * @param progressMonitor
-	 * @throws IOException
-	 */
-	public void generateActionModel(
-			EObject source,
-			URI baseURI,
-			Consumer<Diagnostic> diagnosticConsumer,
-			File actionModelFile,
-			ProgressMonitor progressMonitor) throws IOException {
-		
-		generateActionModel(
-				source,
-				baseURI,
-				diagnosticConsumer, 
-				URI.createFileURI(actionModelFile.getCanonicalFile().getAbsolutePath()), 
-				progressMonitor);
-	}
-	
-	
-	// --- Single source ---	
-	
-	/**
-	 * Generates actions for a single source with a random base URI.
-	 * @param source
-	 * @param nodeProcessorFactory
-	 * @param references
-	 * @param diagnosticConsumer
-	 * @param progressMonitor
-	 * @return
-	 */
-	public Map<EObject,Collection<Label>> generateActionModel(
-			EObject source,
-			Consumer<Diagnostic> diagnosticConsumer,
-			ProgressMonitor progressMonitor) {
-		
-		return generateActionModel(
-				source, 
-				null,
-				diagnosticConsumer,
-				progressMonitor);
-		
-	}	
-
-	/**
-	 * 	 * Generates actions for a single source with a random base URI and saves to a resource at provided URI.
-	 * @param source
-	 * @param nodeProcessorFactory
-	 * @param references
-	 * @param diagnosticConsumer
-	 * @param actionModelResourceURI Action resource URI
-	 * @param progressMonitor
-	 * @throws IOException
-	 */
-	public void generateActionModel(
-			EObject source,
-			Consumer<Diagnostic> diagnosticConsumer,
-			URI actionModelResourceURI,
-			ProgressMonitor progressMonitor) throws IOException {
-	
-		generateActionModel(
-				source,
-				null,
-				diagnosticConsumer,
-				actionModelResourceURI,
-				progressMonitor);
-	}
-
-	/**
-	 * Generates actions for a single source with a random base URI and saves to a file.
-	 * @param source
-	 * @param nodeProcessorFactory
-	 * @param references
-	 * @param diagnosticConsumer
-	 * @param actionModelFile
-	 * @param progressMonitor
-	 * @throws IOException
-	 */
-	public void generateActionModel(
-			EObject source,
-			Consumer<Diagnostic> diagnosticConsumer,
-			File actionModelFile,
-			ProgressMonitor progressMonitor) throws IOException {
-		
-		generateActionModel(
-				source,
-				null,
-				diagnosticConsumer, 
-				URI.createFileURI(actionModelFile.getCanonicalFile().getAbsolutePath()), 
-				progressMonitor);
+	public ArchitectureActionGenerator(EObject source) {
+		this(source, new ArchitectureNodeProcessorFactory(Context.EMPTY_CONTEXT, null));
 	}
 	
 }
