f5563eddb5f8878e9ffe48f0abf36d1594190a6a
Pavel <Pavel.Vlasov@nasdanika.org>
2023-12-22 07:53:30 -0500

    Representation filtering

--- model/src/main/java/org/nasdanika/models/architecture/util/ArchitectureDrawioResource.java
+++ model/src/main/java/org/nasdanika/models/architecture/util/ArchitectureDrawioResource.java
@@ -17,6 +17,7 @@ import org.nasdanika.common.Transformer;
 import org.nasdanika.drawio.emf.DrawioResource;
 import org.nasdanika.drawio.model.ModelFactory;
 import org.nasdanika.drawio.model.util.AbstractDrawioFactory;
+import org.nasdanika.graph.Element;
 import org.nasdanika.persistence.Marker;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
@@ -73,6 +74,11 @@ public class ArchitectureDrawioResource extends ResourceImpl {
 				return ArchitectureDrawioResource.this.getAppBase();
 			}
 			
+			@Override
+			protected void filterRepresentationElement(Element representationElement, EObject semanticElement, ProgressMonitor progressMonitor) {
+				ArchitectureDrawioResource.this.filterRepresentationElement(representationElement, semanticElement, progressMonitor);
+			}
+			
 		};
 		
 		Transformer<EObject,EObject> architectureFactory = new Transformer<>(architectureDrawioFactory);
@@ -122,6 +128,19 @@ public class ArchitectureDrawioResource extends ResourceImpl {
 	protected URI getAppBase() {
 		return AbstractDrawioFactory.DEFAULT_APP_BASE;
 	}
-	
+		
+	/**
+	 * Override to implement filtering of a representation element. 
+	 * For example, if an element represents a processing unit, its background color or image can be modified depending on the load - red for overloaded, green for OK, grey for planned offline.  
+	 * @param representationElement
+	 * @param registry
+	 * @param progressMonitor
+	 */
+	protected void filterRepresentationElement(
+			org.nasdanika.graph.Element representationElement, 
+			EObject semanticElement,
+			ProgressMonitor progressMonitor) {
+		
+	}
 
 }
--- model/src/main/java/org/nasdanika/models/architecture/util/ArchitectureDrawioResourceFactory.java
+++ model/src/main/java/org/nasdanika/models/architecture/util/ArchitectureDrawioResourceFactory.java
@@ -9,6 +9,7 @@ import org.nasdanika.common.NullProgressMonitor;
 import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.drawio.model.ModelFactory;
 import org.nasdanika.drawio.model.util.AbstractDrawioFactory;
+import org.nasdanika.graph.Element;
 import org.nasdanika.persistence.Marker;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
@@ -40,6 +41,11 @@ public class ArchitectureDrawioResourceFactory implements Resource.Factory {
 				return ArchitectureDrawioResourceFactory.this.getAppBase();
 			}
 			
+			@Override
+			protected void filterRepresentationElement(Element representationElement, EObject semanticElement, ProgressMonitor progressMonitor) {
+				ArchitectureDrawioResourceFactory.this.filterRepresentationElement(representationElement, semanticElement, progressMonitor);
+			}
+			
 		};
 	}
 	
@@ -61,6 +67,20 @@ public class ArchitectureDrawioResourceFactory implements Resource.Factory {
 
 	protected URI getAppBase() {
 		return AbstractDrawioFactory.DEFAULT_APP_BASE;
+	}
+	
+	/**
+	 * Override to implement filtering of a representation element. 
+	 * For example, if an element represents a processing unit, its background color or image can be modified depending on the load - red for overloaded, green for OK, grey for planned offline.  
+	 * @param representationElement
+	 * @param registry
+	 * @param progressMonitor
+	 */
+	protected void filterRepresentationElement(
+			org.nasdanika.graph.Element representationElement, 
+			EObject semanticElement,
+			ProgressMonitor progressMonitor) {
+		
 	}
 			
 }
