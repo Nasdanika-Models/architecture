ed17ecbbe29d3d59ec6c7dcf0c021b8282f72db5
Pavel <Pavel.Vlasov@nasdanika.org>
2023-12-26 16:21:18 -0500

    Representation filtering

--- demos/internet-banking-system/src/test/java/org/nasdanika/models/architecture/demos/ibs/tests/TestInternetBankingSystemSiteGen.java
+++ demos/internet-banking-system/src/test/java/org/nasdanika/models/architecture/demos/ibs/tests/TestInternetBankingSystemSiteGen.java
@@ -7,6 +7,7 @@ import java.util.Map.Entry;
 import java.util.function.Consumer;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -19,6 +20,8 @@ import org.nasdanika.common.MutableContext;
 import org.nasdanika.common.PrintStreamProgressMonitor;
 import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.diagramgenerator.plantuml.PlantUMLDiagramGenerator;
+import org.nasdanika.drawio.Node;
+import org.nasdanika.graph.Element;
 import org.nasdanika.html.model.app.gen.ActionSiteGenerator;
 import org.nasdanika.models.architecture.processors.doc.ArchitectureActionGenerator;
 import org.nasdanika.models.architecture.processors.doc.ArchitectureNodeProcessorFactory;
@@ -29,7 +32,29 @@ public class TestInternetBankingSystemSiteGen {
 	@Test
 	public void testGenerateInternetBankingSystemSiteWithMapping() throws Exception {
 		ResourceSet resourceSet = new ResourceSetImpl();
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("drawio", new ArchitectureDrawioResourceFactory(uri -> resourceSet.getEObject(uri, true)));
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("drawio", new ArchitectureDrawioResourceFactory(uri -> resourceSet.getEObject(uri, true)) {
+			
+			@Override
+			protected void filterRepresentationElement(
+					Element representationElement, 
+					EObject semanticElement,
+					Map<EObject, EObject> registry, 
+					ProgressMonitor progressMonitor) {
+
+				super.filterRepresentationElement(representationElement, semanticElement, registry, progressMonitor);
+				
+				if (representationElement instanceof Node) {
+					Node node = (Node) representationElement;
+					if ("MainframeBankingSystemFacadeImpl".equals(node.getId())) {
+						node.getStyle().put("fillColor", "#ffe6cc");
+						node.getStyle().put("strokeColor", "#d79b00");
+					}
+				}					
+			}
+						
+		});
+		
+		
 		File ibsDiagramFile = new File("internet-banking-system.drawio").getCanonicalFile();
 		Resource ibsResource = resourceSet.getResource(URI.createFileURI(ibsDiagramFile.getAbsolutePath()), true);
 		
