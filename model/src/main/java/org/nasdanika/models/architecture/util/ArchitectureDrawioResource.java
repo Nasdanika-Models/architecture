e9532b8f46c420d1d6c849df71bbf08d336b1870
Pavel <Pavel.Vlasov@nasdanika.org>
2024-01-28 02:29:50 -0500

    isRefIdProxyURI

--- model/src/main/java/org/nasdanika/models/architecture/util/ArchitectureDrawioResource.java
+++ model/src/main/java/org/nasdanika/models/architecture/util/ArchitectureDrawioResource.java
@@ -66,8 +66,13 @@ public class ArchitectureDrawioResource extends ResourceImpl {
 		ArchitectureDrawioFactory architectureDrawioFactory = new ArchitectureDrawioFactory() {
 
 			@Override
-			protected EObject getByRefId(String refId, int pass, Map<EObject, EObject> registry) {				
-				return ArchitectureDrawioResource.this.getByRefId(refId, pass, registry);
+			protected EObject getByRefId(EObject eObj, String refId, int pass, Map<EObject, EObject> registry) {				
+				return ArchitectureDrawioResource.this.getByRefId(eObj, getBaseURI(eObj), refId, pass, registry);
+			}
+			
+			@Override
+			protected boolean isRefIdProxyURI() {
+				return true;
 			}
 			
 			@Override
@@ -135,15 +140,19 @@ public class ArchitectureDrawioResource extends ResourceImpl {
 	protected ModelFactory getDrawioFactory() {
 		return org.nasdanika.drawio.model.ModelFactory.eINSTANCE;
 	}	
-	
-	protected EObject getByRefId(String refId, int pass, Map<EObject, EObject> registry) {
+		
+	protected EObject getByRefId(EObject eObj, URI baseURI, String refId, int pass, Map<EObject, EObject> registry) {
 		if (uriResolver == null) {
 			return null;
 		}
 		
 		URI refURI = URI.createURI(refId);
 		if(!getURI().isRelative()) {
-			refURI = refURI.resolve(getURI());
+			if (baseURI == null) {
+				refURI = refURI.resolve(getURI());
+			} else {
+				refURI = refURI.resolve(baseURI);
+			}
 		}
 		return uriResolver.apply(refURI);
 	}
