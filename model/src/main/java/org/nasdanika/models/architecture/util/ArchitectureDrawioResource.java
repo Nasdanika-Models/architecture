16089c178531d32fedacccc45c977727681cc436
Pavel <Pavel.Vlasov@nasdanika.org>
2024-05-24 20:19:12 -0400

    Thread context class loader

--- model/src/main/java/org/nasdanika/models/architecture/util/ArchitectureDrawioResource.java
+++ model/src/main/java/org/nasdanika/models/architecture/util/ArchitectureDrawioResource.java
@@ -172,7 +172,7 @@ public class ArchitectureDrawioResource extends ResourceImpl {
 	}
 	
 	protected ClassLoader getClassLoader(EObject context, URI baseURI, Supplier<ClassLoader> logicalParentClassLoaderSupplier) {
-		return logicalParentClassLoaderSupplier == null ? getClass().getClassLoader() : logicalParentClassLoaderSupplier.get();
+		return logicalParentClassLoaderSupplier == null ? Thread.currentThread().getContextClassLoader() : logicalParentClassLoaderSupplier.get();
 	}	
 	
 	protected URI getAppBase() {
--- model/src/main/java/org/nasdanika/models/architecture/util/ArchitectureDrawioResourceFactory.java
+++ model/src/main/java/org/nasdanika/models/architecture/util/ArchitectureDrawioResourceFactory.java
@@ -89,7 +89,7 @@ public class ArchitectureDrawioResourceFactory implements Resource.Factory {
 	}	
 	
 	protected ClassLoader getClassLoader(EObject context, URI baseURI, Supplier<ClassLoader> logicalParentClassLoaderSupplier) {
-		return logicalParentClassLoaderSupplier == null ? getClass().getClassLoader() : logicalParentClassLoaderSupplier.get();
+		return logicalParentClassLoaderSupplier == null ? Thread.currentThread().getContextClassLoader() : logicalParentClassLoaderSupplier.get();
 	}	
 
 	protected URI getAppBase() {
